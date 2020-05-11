eb05a63db567204794e4d2b0a7ad0472bcc446af
Luka Radovanovic <lukadlm97@gmail.com>
2020-05-11 22:10:01 +0200

    implemented data persistent service and all user data service

---
+++ FootballStatistics.ConsoleAppTesting/Program.cs
@@ -0,0 +1,21 @@
+﻿using FootballStatistics.Domain.Models;
+using FootballStatistics.Domain.Services;
+using FootballStatistics.EntityFramework;
+using FootballStatistics.EntityFramework.Services;
+using System;
+using System.Collections.Generic;
+
+namespace FootballStatistics.ConsoleAppTesting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IDataService<User> userDataService = new UserDataService(new FootballStatisticsDbContextFactory());
+
+            Console.WriteLine(userDataService.Get(1).Result);
+
+            Console.ReadKey();
+        }
+    }
+}
