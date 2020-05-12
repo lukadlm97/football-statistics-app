616d3a3b4cc8668a9f75c28299c0a8af1ed9a34d
Luka Radovanovic <lukadlm97@gmail.com>
2020-05-13 00:38:49 +0200

    witten first test but debuger not responding

---
+++ FootballStatistics.Domain.Test/Services/AuthentificationServices/AuthentificationServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using FootballStatistics.Domain.Models;
+using FootballStatistics.Domain.Services;
+using FootballStatistics.Domain.Services.AuthenticationService;
+using Microsoft.AspNet.Identity;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballStatistics.Domain.Test.Services.AuthentificationServices
+{
+    [TestFixture]
+    public class AuthentificationServiceTest
+    {
+        [Test]
+        public async void Login_WithTheCorrectPasswordForExistingUsername_ReturnUserFroCorrectUsername()
+        {
+            //Arrange
+            string expectedUsername = "testuser";
+            string password = "testpassword";
+            Mock<IDataService<User>> mockUserService = new Mock<IDataService<User>>();
+            mockUserService.Setup(s => s.GetByUsername(expectedUsername)).ReturnsAsync(new User { Username = expectedUsername });
+            Mock<IPasswordHasher> mockPasswordHasher = new Mock<IPasswordHasher>();
+            mockPasswordHasher.Setup(s => s.VerifyHashedPassword(It.IsAny<string>(), password)).Returns(PasswordVerificationResult.Success);
+            AuthenticationService authenticationService = new AuthenticationService(mockUserService.Object,mockPasswordHasher.Object);
+            
+
+            //Act
+            User u = await authenticationService.Login(expectedUsername, password);
+
+            //Assert
+            string actualUsername = u.Username;
+            Assert.AreEqual(expectedUsername,actualUsername);
+        }
+    }
+}
